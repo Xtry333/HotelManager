23bb22c00d2f394018f8cdbee67752f7d10b3ebf
Michał Woźniak <michalw@kainos.com>
2019-10-19 21:26:20 +0200

    Client React Typescript

---
+++ client-react/src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Hello } from "./components/Hello";
+
+import "./style.less";
+
+export interface AppProps { title: "React App!" }
+
+export class App extends React.Component<AppProps, {}> {
+    componentDidMount() {
+        console.info(`${this.props.title}`);
+    };
+
+    render() {
+        return (
+            <div id="App">
+                <Hello compiler="TypeScript" framework="React" />
+            </div>
+        );
+    }
+}
---
+++ client-react/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/Hello";
+import { App } from "./App";
+
+ReactDOM.render(
+    <App title="React App!" />,
+    document.getElementById("root")
+);

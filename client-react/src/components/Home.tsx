00b607ccd3593ad2eb3ae06478c78650ebcdf27e
Michał Woźniak <michalw@kainos.com>
2019-11-04 15:46:59 +0100

    Main menu buttons

--- client-react/src/components/Home.tsx
+++ client-react/src/components/Home.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
 
-class Home extends React.Component {
+class Home extends React.Component<RouteComponentProps> {
     render() {
         const user = JSON.parse(localStorage.getItem('user')) || {};
+        const token = localStorage.getItem('token') || '';
         return (
             <div className='Home'>
-                <header className="Home-header ui centered header">
+                <header className="ui centered header">
                     <h2>Hello {user.firstname}</h2>
                     <p>Choose your path</p>
+                    <div className="ui buttons">
+                        <button className="ui teal button" onClick={e => this.props.history.push(`/guests/create/`)}>
+                            Create Guest
+                        </button>
+                        <button className="ui teal button" onClick={e => this.props.history.push(`/reservations/create/`)}>
+                            Create Reservation
+                        </button>
+                    </div>
                 </header>
             </div>
         );

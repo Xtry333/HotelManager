900289941c88342933e5da7d5caf07737ffd6778
Michał Woźniak <michalw@kainos.com>
2020-02-17 11:19:28 +0100

    UsersRouter

--- server/routes/api/auth/users.ts
+++ server/routes/api/auth/users.ts
@@ -1,9 +1,27 @@
 import Express, { Router } from 'express';
-var router = Express.Router();
+import * as UserController from '../../../controllers/UserController';
+
+const router = Express.Router();
 
 /* GET users listing. */
-router.get('/', function(req: Express.Request, res: Express.Response, next: Express.NextFunction) {
-    res.json({message: 'respond with a resource'});
+router.get('/', async (req, res, next) => {
+    try {
+        const guests = await UserController.getAll();
+        res.json(guests);
+    } catch (error) {
+        res.status(error.status).json(error)
+    }
+});
+
+/* GET user with id. */
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const guest = await UserController.getById(id);
+        res.status(200).json(guest);
+    } catch (error) {
+        res.status(error.status).json(error);
+    }
 });
 
 export default router;

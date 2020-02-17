89118e469c41c96b8bb8a582d510584203baddbc
Michał Woźniak <michalw@kainos.com>
2020-02-17 13:07:36 +0100

    Tests fixes and cleanup

--- server/tests/validator.test.ts
+++ server/tests/validator.test.ts
@@ -2,12 +2,18 @@ import { expect } from 'chai';
 import * as Validator from '../js/validator';
 
 describe('Validator', () => {
-    it('returns true when email is correct', () => {
+    it('correctly validates isEmpty', () => {
+        expect(Validator.isEmpty(undefined as any)).to.be.true;
+        expect(Validator.isEmpty(null as any)).to.be.true;
+        expect(Validator.isEmpty('')).to.be.true;
+        expect(Validator.isEmpty('stuff')).to.be.false;
+        expect(Validator.isEmpty('a')).to.be.false;
+    });
+    
+    it('correctly validates Email', () => {
         expect(Validator.validateEmail('e@mail.com')).to.be.true;
         expect(Validator.validateEmail('mailer@mmmmail.pl')).to.be.true;
-    });
 
-    it('returns false when email is not correct', () => {
         expect(Validator.validateEmail('e@mail.')).to.be.false;
         expect(Validator.validateEmail('@mmmmail.pl')).to.be.false;
         expect(Validator.validateEmail('iamamail.com')).to.be.false;

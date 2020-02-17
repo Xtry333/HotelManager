810518dadf44f38b0d75d2bfc572a1e3ab1a5ae7
Michał Woźniak <michalw@kainos.com>
2020-02-17 14:36:55 +0100

    Adding length validator

--- server/js/validator.ts
+++ server/js/validator.ts
@@ -1,17 +1,21 @@
 function validate(value: string, type: string) {
     switch (type) {
-        case 'email': 
+        case 'email':
             return validateEmail(value);
     }
 }
 
-export function isEmpty(value: string) {
+export function isEmpty(value: string): boolean {
     return !(value && value.length > 0);
 }
 
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export function validateEmail(value: string) {
+export function validateEmail(value: string): boolean {
     return emailRegex.test(value);
+}
+
+export function validateLength(value: string, length: { min: number, max: number }): boolean {
+    return !!value && value.length >= length.min && value.length <= length.max;
 }
 
 export default validate;
--- server/tests/validator.test.ts
+++ server/tests/validator.test.ts
@@ -9,7 +9,7 @@ describe('Validator', () => {
         expect(Validator.isEmpty('stuff')).to.be.false;
         expect(Validator.isEmpty('a')).to.be.false;
     });
-    
+
     it('correctly validates Email', () => {
         expect(Validator.validateEmail('e@mail.com')).to.be.true;
         expect(Validator.validateEmail('mailer@mmmmail.pl')).to.be.true;
@@ -17,5 +17,14 @@ describe('Validator', () => {
         expect(Validator.validateEmail('e@mail.')).to.be.false;
         expect(Validator.validateEmail('@mmmmail.pl')).to.be.false;
         expect(Validator.validateEmail('iamamail.com')).to.be.false;
+    });
+
+    it('correctly checks length of string', () => {
+        const length = {min: 3, max: 8};
+        expect(Validator.validateLength('a cookie', length)).to.be.true;
+        expect(Validator.validateLength('a cookiee', length)).to.be.false;
+        expect(Validator.validateLength('a', length)).to.be.false;
+        expect(Validator.validateLength(undefined as any, length)).to.be.false;
+        expect(Validator.validateLength(null as any, length)).to.be.false;
     });
 });

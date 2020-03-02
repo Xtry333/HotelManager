652b213230b77ade12fd8804ac00e0f8c2ebbaff
Michał Woźniak <michalw@kainos.com>
2020-03-02 17:23:55 +0100

    Env prefixed with DB

--- server/src/common/sql.ts
+++ server/src/common/sql.ts
@@ -1,9 +1,9 @@
 import mysql from 'mysql2';
 
 const config = {
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 }
 
 const pool = mysql.createPool({
